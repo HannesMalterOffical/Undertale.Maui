9b1bcf2656a716cc3a92dcac2d4d379824207ff1
Mihkel Hain <Mihkelhain7@gmail.com>
2023-04-17 15:12:58 +0300

    Added a button to SearchPage

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Undertale.Maui;
+﻿using Views;
+
+namespace Undertale.Maui;
 
 public partial class App : Application
 {
@@ -6,6 +8,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new NavigationPage(new BeginningPage());
+    }
 }

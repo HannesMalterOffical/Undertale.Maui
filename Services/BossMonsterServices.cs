07d714644697f61afa20d8f5d4819a4b7d04ef82
HannesMalter <hannesmalter1234@gmail.com>
2023-04-19 15:33:51 +0300

    Added Colors and styles, GlobalUsing

--- Services/BossMonsterServices.cs
+++ Services/BossMonsterServices.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Undertale.Maui.Models;
-
-namespace Undertale.Maui.Services
+﻿namespace Undertale.Maui.Services
 {
     internal class BossMonsterServices
     {

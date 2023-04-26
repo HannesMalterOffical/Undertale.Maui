3f92d4e87673a7d53cc07e72f5d1e1540b668139
HannesMalter <hannesmalter1234@gmail.com>
2023-04-26 15:41:53 +0300

    fIXES

--- Views/SearchPage.xaml.cs
+++ Views/SearchPage.xaml.cs
@@ -4,7 +4,6 @@ namespace Views;
 
 public partial class SearchPage : ContentPage
 {
-	
     public SearchPage()
     {
         InitializeComponent();
@@ -15,18 +14,14 @@ public partial class SearchPage : ContentPage
         base.OnAppearing();
 
         AllMonsterLst.ItemsSource = MonsterServices.GetAllMonsters();
+
+        AllBossMonsterLst.ItemsSource = BossMonsterServices.GetAllBossMonsters();
     }
 
     async void Characters_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
     {
         await Navigation.PushAsync(new EndPage(e.CurrentSelection.First() as Monsters));
     }
-
-    //async void Characters_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
-    //{
-    //    await Navigation.PushAsync(new EndPage(e.CurrentSelection.First() as Monsters));
-    //}
-
 
     async void GridArea_Tapped(System.Object sender, System.EventArgs e)
     {

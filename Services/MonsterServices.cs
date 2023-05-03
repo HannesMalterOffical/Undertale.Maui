20dcd65acaf0271f981aad18f8318db9d4c9114a
opilane <opilane@FUJI-YMHP016307>
2023-05-03 14:29:03 +0300

    end page fix

--- Services/MonsterServices.cs
+++ Services/MonsterServices.cs
@@ -9,7 +9,8 @@
                 Name = " Froggit",
                 Quote = "Ribbit, ribbit.",
                 CharacterImage = "Froggit.png",
-                Description = "Life is difficult for this enemy.",
+                Description = "Life is difficult for this enemy. " +
+                "\r\n\r\nAppearance\r\nFroggit's overall appearance is that of a white frog. Only its two front feet are visible, with three black dots on its chest. Between its two front feet is what appears to be some small creature with eyes that excessively blink and a rounded mouth. It is unknown what this creature is, as it is never mentioned in flavor text or by any Froggits.\r\n\r\nPersonality\r\nFroggit is overall a weak monster, attempting to run away from the protagonist when at low health, shivering when threatened and leaving after Toriel glares at it for attacking the protagonist. It is influenced easily by the protagonist's comments, despite not being able to understand them. However, it is also somewhat informed, telling the protagonist about functions in the game and foreshadowing sparing techniques.",
                 AccentColorStart = Color.FromArgb("#000000"),
                 AccentColorEnd = Color.FromArgb("#000000"),
                 Images = new()
